bdf7d84a7df536488c00a29ba67541b125eb27cc
Yogendra Jhala <yogendrajhala650@gmail.com>
2022-06-15 23:19:52 +0530

    First commit

---
+++ src/components/Footer.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Footer() {
+  const year = new Date().getFullYear();
+  return (
+    <footer>
+      <p>Hopefully you love sarcasm</p>
+    </footer>
+  );
+}
+
+export default Footer;
